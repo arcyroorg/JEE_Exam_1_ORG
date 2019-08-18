3a056256736e10751c8eff957f3f0b16fb24b5ed
arek-jozwiak-coderslab <Gonsie123>
2019-08-18 10:07:35 +0200

    first commit

---
+++ exam/Main2.java
@@ -0,0 +1,20 @@
+package exam;
+
+public class Main2 {
+	
+	public static void main (String[] args){
+		
+		String str = "Naucz się programować od podstaw";
+		
+		System.out.println(shorten(str, 2));
+
+	}
+
+	
+	static String shorten(String str, int length){
+	    return str.substring(0, length);
+		
+		
+	}
+	
+}
